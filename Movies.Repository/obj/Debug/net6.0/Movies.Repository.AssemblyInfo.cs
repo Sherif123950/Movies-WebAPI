c415fedf06bc7a454d19dcb9df2f3c928cba2938
Sherif123950 <68925755+Sherif123950@users.noreply.github.com>
2023-12-22 14:18:38 +0200

    Final Commit

--- Movies.Repository/obj/Debug/net6.0/Movies.Repository.AssemblyInfo.cs
+++ Movies.Repository/obj/Debug/net6.0/Movies.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e602d4c91de59897789139abbb673d46559fd4e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
